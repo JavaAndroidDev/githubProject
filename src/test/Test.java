3ae327cd6b0177a98a27e34b14b2fb7458b3fbf5
JavaAndroidDeveloper <omanahmed14@gmail.com>
2016-12-11 12:28:28 +0600

    Uploaded by JavaAndroidDev

---
+++ src/test/Test.java
@@ -0,0 +1,130 @@
+package test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.UnsupportedLookAndFeelException;
+import jdk.nashorn.internal.ir.ContinueNode;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagLayout;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+/**
+ * Code to produce from 00:00 to 24:59 
+ */        
+        
+//        String i = "0";
+//        for(int hour=0;hour<=24;hour++){            
+//            for(int min=0;min<=59;min++){
+//                if(hour<=9 && min<=59){
+//                    System.out.println(i+hour+":"+min);
+//                }
+//                else
+//                    System.out.println(hour+":"+min);
+//            }
+//        }
+        
+        //End of the code.
+    }
+        
+/**
+ * Decimal to binary converter.
+ */
+         
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int n = reader.nextInt();
+//        String s = Integer.toString(n);
+//        Integer intWrapper = Integer.valueOf("10010", 2);
+//        System.out.println(intWrapper);
+//
+//        int number;
+//
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Enter only positive Number: ");
+//        number = in.nextInt();
+//
+//        if (number < 0) {
+//            
+//            System.out.println("Error: Not a positive integer");
+//
+//        }
+//        else{
+//
+//            System.out.print("Convert to Binary is: ");
+//            //System.out.print(binaryform(number));
+//            printBinaryform(number);
+//            System.out.println("\n");
+//            
+//        }
+//        
+//    }
+//    
+//    private static void printBinaryform(int number) {
+//        int remainder;
+//
+//        if (number <= 1) {
+//            System.out.print(number);
+//            return;   // KICK OUT OF THE RECURSION
+//        }
+//
+//        remainder = number % 2;
+//        printBinaryform(number >> 1);
+//        System.out.print(remainder);
+//    }
+    
+//Upto this line of code.
+    
+/**
+ * Program to show system date and time.
+ */
+        
+//        String months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+//            "Sep", "Oct", "Nov", "Dec"};
+//
+//        Calendar calendar = Calendar.getInstance();
+//
+//        System.out.print("Date: ");
+//        System.out.print(months[calendar.get(Calendar.MONTH)]);
+//        System.out.print(" " + calendar.get(Calendar.DATE) + " ");
+//        System.out.println(calendar.get(Calendar.YEAR));
+//
+//        System.out.print("Time: ");
+//        System.out.print(calendar.get(Calendar.HOUR) + ":");
+//        System.out.print(calendar.get(Calendar.MINUTE) + ":");
+//        System.out.println(calendar.get(Calendar.SECOND));
+//
+//        calendar.set(Calendar.HOUR, 10);
+//        calendar.set(Calendar.MINUTE, 29);
+//        calendar.set(Calendar.SECOND, 22);
+//
+//        System.out.print("Updated time: ");
+//        System.out.print(calendar.get(Calendar.HOUR) + ":");
+//        System.out.print(calendar.get(Calendar.MINUTE) + ":");
+//        System.out.println(calendar.get(Calendar.SECOND));
+        
+//Upto this line of code.
+    
+}
